9dfe6894a7307e39c0086cfc0666659a37c6cf19
= <=>
2016-10-09 17:29:39 +0600

    triangle tested
    triangle tested

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,7 @@ import { Circle } from './src/Circle.js';
 import { Parallelogram } from './src/Parallelogram.js';
 import { Rectangle } from './src/Rectangle.js';
 import { Square } from './src/Square.js';
-import { Triangleup } from './src/Triangleup.js';
+import { Triangle } from './src/Triangle.js';
 
 var circles = document.getElementsByTagName('circle');
 for (var i = 0; i < circles.length; i++) {
@@ -28,8 +28,52 @@ for (var i = 0; i < squares.length; i++){
 	window.onload = Square.processSquare(squares[i]);
 }
 
-var trianglesup = document.getElementsByTagName('triangle-up');
 
-for (var i = 0; i < trianglesup.length; i++){
-	window.onload = Triangleup.processTriangle(trianglesup[i]);
+var triangleUps = document.getElementsByTagName('triangle-up');
+for (var i = 0; i < triangleUps.length; i++){
+    window.onload = Triangle.processingTriangle(triangleUps[i], 'up');
 }
+
+var triangleLefts = document.getElementsByTagName('triangle-left');
+for (var i = 0; i < triangleLefts.length; i++){
+    window.onload = Triangle.processingTriangle(triangleLefts[i], 'left');
+}
+
+var triangleRights = document.getElementsByTagName('triangle-right');
+for (var i = 0; i < triangleRights.length; i++){
+    window.onload = Triangle.processingTriangle(triangleRights[i], 'right');
+}
+
+var triangleDowns = document.getElementsByTagName('triangle-down');
+for (var i = 0; i < triangleDowns.length; i++){
+    window.onload = Triangle.processingTriangle(triangleDowns[i], 'down');
+}
+
+var triangleTopLeft = document.getElementsByTagName('triangle-top-left');
+for (var i = 0; i < triangleTopLeft.length; i++){
+    window.onload = Triangle.processingTriangle(triangleTopLeft[i], 'top-left');
+}
+var triangleTopRight = document.getElementsByTagName('triangle-top-right');
+for (var i = 0; i < triangleTopRight.length; i++){
+    window.onload = Triangle.processingTriangle(triangleTopRight[i], 'top-right');
+}
+
+var triangleBottomRight = document.getElementsByTagName('triangle-bottom-right');
+for (var i = 0; i < triangleBottomRight.length; i++){
+    window.onload = Triangle.processingTriangle(triangleBottomRight[i], 'bottom-right');
+}
+
+var triangleBottomLeft = document.getElementsByTagName('triangle-bottom-left');
+for (var i = 0; i < triangleBottomLeft.length; i++){
+    window.onload = Triangle.processingTriangle(triangleBottomLeft[i], 'bottom-left');
+}
+
+
+var triangles = document.getElementsByTagName('triangle');
+for (var i = 0; i < triangles.length; i++){
+	window.onload = Triangle.processingTriangle(triangles[i], null);
+}
+
+const test = new Triangle('I am triangle');
+
+console.log(test.gettingValue());
--- resources/assets/js/src/triangle.js
+++ resources/assets/js/src/triangle.js
@@ -1,240 +1,124 @@
+export class Triangle{
 
-// TRIANGLE-UP START
+    static processingTriangle(triangle, direction){
 
-var triangles = document.getElementsByTagName('triangle-up');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
 
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
+        function getShapeLength(shape){
+            var width = triangle.getAttribute(shape);
+            var value = width.replace(/[^0-9]/g, '');
+            var unit = width.replace(/[0-9]/g, '');
 
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
+            if (unit == '%'){
+                unit = 'em';
+            }
+            return value+unit;
+        }
+
+        function defaultValue(){
+            var style = window.getComputedStyle(triangle);
+            var value = style.getPropertyValue('border-bottom');
+            return value.split(' ')[0];
+        }
+
+        function borderWidth(){
+           return triangle.hasAttribute('shape-width') ? getShapeLength('shape-width') :defaultValue();
+        }
+
+        function borderHeight(){
+            return triangle.hasAttribute('shape-height') ? getShapeLength('shape-height') : defaultValue();
+        }
+
+        function getBackground(){
+            var valueColor= triangle.hasAttribute('shape-background')? triangle.getAttribute('shape-background') : defaultBackground();
+
+            if(direction == null || direction == 'up'){
+                return ['transparent', 'transparent', valueColor];
+
+            }
+
+            if(direction == 'left'){
+                return ['transparent', valueColor, 'transparent'];
+            }
+
+            if(direction == 'right'){
+                return [ valueColor, 'transparent' , 'transparent'];
+            }
+
+            if(direction == 'down'){
+                return ['transparent', valueColor, 'transparent'];
+            }
+            if(direction == 'top-left'){
+                return [valueColor, 'transparent'];
+            }
+
+            if(direction == 'top-right'){
+                return ['transparent', valueColor ];
+            }
+
+            if(direction == 'bottom-right'){
+                return ['transparent', valueColor ];
+            }
+
+            if(direction == 'bottom-left'){
+                return ['transparent', valueColor ];
+            }
+
+        }
+
+
+
+
+        function defaultBackground(){
+            var style = window.getComputedStyle(triangle);
+            var value = style.getPropertyValue('border-bottom');
+            return value.replace(value.split(' ')[0], '').replace(value.split(' ')[1], '');
+        }
+
+       if(direction==null || direction=='up'){
+           triangle.style.borderRight = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderLeft = borderWidth() + ' solid ' + getBackground()[1];
+           triangle.style.borderBottom = borderHeight() + ' solid ' + getBackground()[2];
+       }
+
+        if(direction=='left'){
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderRight = borderWidth() + ' solid ' + getBackground()[1];
+           triangle.style.borderBottom = borderHeight() + ' solid ' + getBackground()[2];
+       }
+
+        if(direction=='right'){
+           triangle.style.borderLeft = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[1];
+           triangle.style.borderBottom = borderHeight() + ' solid ' + getBackground()[2];
+       }
+
+        if(direction=='down'){
+           triangle.style.borderLeft = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[1];
+           triangle.style.borderRight = borderHeight() + ' solid ' + getBackground()[2];
+       }
+
+        if(direction=='top-left'){
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderRight = borderHeight() + ' solid ' + getBackground()[1];
+       }
+
+        if(direction=='top-right'){
+           triangle.style.borderBottom = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderRight = borderHeight() + ' solid ' + getBackground()[1];
+       }
+
+        if(direction=='bottom-right'){
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderRight = borderHeight() + ' solid ' + getBackground()[1];
+       }
+
+        if(direction=='bottom-left'){
+           triangle.style.borderTop = borderWidth() + ' solid ' + getBackground()[0];
+           triangle.style.borderLeft = borderHeight() + ' solid ' + getBackground()[1];
+       }
+
+
     }
-    else{
-    	height = width;
-    }
 
-	triangle.style.borderLeft = width +' '+fixedAttr;
-	triangle.style.borderRight = width +' '+fixedAttr;
-	triangle.style.borderBottom = height +' solid '+color;
 }
-
-// TRIANGLE-UP END
-
-
-
-
-//------------------------*******************---------------------------//
-
-// TRIANGLE-DOWN START
-
-var triangles = document.getElementsByTagName('triangle-down');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	triangle.style.borderLeft = width +' '+fixedAttr;
-	triangle.style.borderRight = width +' '+fixedAttr;
-	triangle.style.borderTop = height +' solid '+color;
-}
-
-// TRIANGLE-DOWN END
-
-//---------------------------------------------------//
-
-
-
-
-// TRIANGLE-RIGHT END
-
-//--------------------------******************-------------------------//
-
-// TRIANGLE-DOWN START
-
-var triangles = document.getElementsByTagName('triangle-right');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	triangle.style.borderTop = width +' '+fixedAttr;
-	triangle.style.borderRight = width +' '+fixedAttr;
-	triangle.style.borderLeft = height +' solid '+color;
-}
-
-// TRIANGLE-DOWN END
-
-
-//-----------------------*****************----------------------------//
-
-
-// TRIANGLE-LEFT START
-
-var triangles = document.getElementsByTagName('triangle-left');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	triangle.style.borderTop = width +' '+fixedAttr;
-	triangle.style.borderLeft = width +' '+fixedAttr;
-	triangle.style.borderRight = height +' solid '+color;
-}
-
-// TRIANGLE-LEFT END
-
-//---------------------************------------------------------//
-
-
-// TRIANGLE-TOP-LEFT START
-
-var triangles = document.getElementsByTagName('triangle-top-left');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	triangle.style.borderRight = width +' '+fixedAttr;	
-	triangle.style.borderTop = height +' solid '+color;
-}
-
-// TRIANGLE-TOP-LEFT  END
-
-//---------------------************------------------------------//
-
-
-
-// TRIANGLE-TOP-RIGHT START
-
-var triangles = document.getElementsByTagName('triangle-top-right');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	
-	triangle.style.borderBottom = width +' '+fixedAttr;
-	triangle.style.borderRight = height +' solid '+color;
-}
-
-// TRIANGLE-TOP-RIGHT  END
-
-//---------------------************------------------------------//
-
-
-//---------------------************------------------------------//
-
-
-
-// TRIANGLE-BOTTOM-RIGHT START
-
-var triangles = document.getElementsByTagName('triangle-bottom-right');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	
-	triangle.style.borderTop = width +' '+fixedAttr;
-	triangle.style.borderRight = height +' solid '+color;
-}
-
-// TRIANGLE-BOTTOM-RIGHT  END
-
-//---------------------************------------------------------//
-
-
-//---------------------************------------------------------//
-
-
-
-// TRIANGLE-BOTTOM-LEFT START
-
-var triangles = document.getElementsByTagName('triangle-bottom-left');
-for (var i = 0; i < triangles.length; i++) {
-	var triangle = triangles[i];
-
-var width = triangle.getAttribute('shape-width'),
-	height = triangle.getAttribute('shape-height'),
-	color = triangle.getAttribute('shape-color'),
-	fixedAttr = 'solid transparent';
-
-	if (triangle.hasAttribute('shape-height')) {
-        height = height;
-    }
-    else{
-    	height = width;
-    }
-
-	
-	triangle.style.borderTop = width +' '+fixedAttr;
-	triangle.style.borderLeft = height +' solid '+color;
-}
-
-// TRIANGLE-BOTTOM-lEFT  END
-
-//---------------------************------------------------------//
